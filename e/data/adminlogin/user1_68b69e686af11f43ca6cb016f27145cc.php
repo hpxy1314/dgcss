68d84f10d2d5277ef6f2e3bcdf95291b86e6b5a4
hp <57720727@qq.com>
2015-02-03 13:56:18 +0800

    v1

---
+++ e/data/adminlogin/user1_68b69e686af11f43ca6cb016f27145cc.php
@@ -0,0 +1,12 @@
+<?php
+$ecms_adminloginr=array();
+$ecms_adminloginr=Array('userid'=>'1',
+'ernd'=>'xHpuhuKuln4MtLiruxJfkWyuBoO',
+'erndtwo'=>'9uKK6tesqOt22CgyEZGj',
+'erndadd'=>'ZxtIOpf7JG49eidJQVEjhTnBMUBmuExh',
+'ehash'=>'g0lVxjJKAmUS2YyuBaRi',
+'ehashname'=>'ehash_kHo7',
+'rhash'=>'7lUvFWOotJP7',
+'rhashname'=>'rhash_dE9R',
+'edbdata'=>'1');
+?>
